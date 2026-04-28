d5b1cb5d4aab6cc45082081584da936142e01a6a
gucio321 <73652197+gucio321@users.noreply.github.com>
2026-04-28 15:49:16 +0200

    header: fix build issues

--- 003/solver/solver.h
+++ 003/solver/solver.h
@@ -1,13 +1,24 @@
 #pragma once
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
 
 struct Vector2D {
 	float x;
 	float y;
 
+        Vector2D(float x_, float y_) : x(x_), y(y_) {}
         Vector2D operator*(const float& scalar);
         Vector2D operator/(const float& scalar);
 };
 
 void dfdx(Vector2D*, Vector2D*, Vector2D*, Vector2D*, float, size_t);
-Vector2D sin(Vector2D);
 Vector2D operator*(float, Vector2D);
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+Vector2D sin(Vector2D);
